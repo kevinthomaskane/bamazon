b4c65578335a379cb1e14d67bee2cdfec7d0ae42
Kevin Kane <kevinthomaskane@gmail.com>
2018-03-18 10:29:17 -0700

    created starter files and installed npm packages

---
+++ bamazonCustomer.js
@@ -0,0 +1,17 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer")
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  user: "root",
+
+  password: "",
+  database: "bamazon"
+});
+
+connection.query("SELECT * FROM products", function(err, res){
+    if (err) throw err;
+    console.log(res)
+})
