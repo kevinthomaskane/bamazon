0c8021f93af1af54c0ace4e301317e164c5fbe1c
Kevin Kane <kevinthomaskane@gmail.com>
2018-03-18 16:19:56 -0700

    new dept feature functional

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer")
+var cTable = require('console.table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -48,16 +49,44 @@ function salesByDept(dept){
             sum += array[i].product_sales;
         }
         var profit = sum - array[0].over_head_costs
-        console.log("department_id | department_name | over_head_costs | product_sales | total_profit ")
-        console.log("---------------------------------------------------------------------------------")
-        console.log("   "+ array[0].id+ "        " + "        " + array[0].department_name + "      " + "       " + array[0].over_head_costs + "       "+ "       " + sum +"          " + profit)
+        console.table([
+            {
+                department_id: array[0].id,
+                department_name: array[0].department_name,
+                over_head_costs: array[0].over_head_costs,
+                product_sales: sum,
+                total_profit: profit
+            }
+        ])
     })
   
         var sum = 0;
         for (let i = 0; i<res.length; i++){
             sum += res[i].product_sales
         }  
-    setTimeout(start, 3000)
+    setTimeout(start, 5000)
+}
+
+function newDept(name, cost){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of the department?"
+        },
+        {
+            type: "input",
+            name: "cost",
+            message:"What is the overhead of this department?"
+        }
+    ]).then(function(answer){
+        connection.query("INSERT INTO departments SET ?", {department_name: answer.name, over_head_costs: answer.cost}, function(err, res){
+            console.log("department added")
+            setTimeout(start, 3000)
+        })
+    })
+    
+    
 }
 
 function start(){
@@ -67,13 +96,14 @@ function start(){
             name: "choice",
             message: "What would you like to do?",
             choices: ["View product sales by department", "Create new department"]
-        }
+        },
+
     ]).then(function(answer){
         if (answer.choice === "View product sales by department"){
             readDept();
         }
         if (answer.choice === "Create new department"){
-            
+            newDept();
         }
     })
 }
